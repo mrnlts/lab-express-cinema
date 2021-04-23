2b6dd4a7f4081a5614d50007882c05d28c42f286
marina <marinaaltesmagret@gmail.com>
2021-04-23 18:39:27 +0200

    Iteration 1

--- configs/db.config.js
+++ configs/db.config.js
@@ -6,7 +6,5 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
